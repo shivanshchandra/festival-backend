865c7ddd3e5129c500c3fdee5034d79345a0efec
Shivansh Chandra <shivansh.chandra96@gmail.com>
2026-04-10 18:41:08 +0530

    Initial backend setup with Spring Boot and PostgreSQL

---
+++ src/main/kotlin/com/app/festival_backend/FestivalBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.app.festival_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FestivalBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FestivalBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/festival_backend/FestivalBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.festival_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FestivalBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
